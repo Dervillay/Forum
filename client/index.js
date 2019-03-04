19ea2cee0f6b740711e5f63b1e55a56c7719be62
Dervillay <james.gascoigneburns7@gmail.com>
2019-03-04 12:01:34 +0000

    Add more comments

--- client/index.js
+++ client/index.js
@@ -1,17 +1,22 @@
+// Refresh and update page when refresh button clicked
 document.getElementById("refresh").addEventListener("click", async function(event) {
 
+  // Uses GET method 'users' to receive list of users
   let usersResponse = await fetch("http://127.0.0.1:8090/users");
   let usersBody = await usersResponse.text();
 
+  // Uses GET method 'messages' to receive list of messages
   let messagesResponse = await fetch("http://127.0.0.1:8090/messages");
   let messagesBody = await messagesResponse.text();
 
-
+  // Creates <ul> tag with id 'posts' in main body of HTML
   document.getElementById("content").innerHTML = "<ul id=\"posts\">";
 
+  // Parses data received by GET methods into JS objects
   let usersPost = JSON.parse(usersBody);
   let messagesPost = JSON.parse(messagesBody);
 
+  // Iterates through users and messages and prduces forum posts using their information
   for (let i = 0; i < usersPost.length; i++) {
     document.getElementById("content").innerHTML +=
     `
@@ -29,13 +34,14 @@ document.getElementById("refresh").addEventListener("click", async function(even
     </div>
     </a>
     </li>
-    `;
+    `
   };
 
   document.getElementById("content").innerHTML += "</ul>";
 
 });
 
+// Searches page for posts with content matching query string
 function searchPage() {
 
   // Declares all relevant variables
@@ -65,11 +71,13 @@ function searchPage() {
   }
 }
 
+// Opens pop-up form
 function openForm() {
   document.getElementById("form").style.display = "block";
   document.getElementById("makePost").style.display = "none";
 }
 
+// Closes pop-up form
 function closeForm() {
   document.getElementById("form").style.display = "none";
   document.getElementById("makePost").style.display = "block";
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
+// Initial setup
 const express = require('express');
 const app = express();
 
 app.use(express.static('client'));
 app.use(express.urlencoded());
 
+// Defines intially empty lists: users and messages
 let users = [];
 let messages = [];
 
+// Adds user and message to respective lists
 app.post('/add', function(req, res) {
   users.push(req.body.username);
   messages.push(req.body.message);
-  console.log(users, messages);
 });
 
+// Gets list of users
 app.get('/users', function(req, res) {
   res.send(users);
 })
 
+// Gets list of messages
 app.get('/messages', function(req, res) {
   res.send(messages);
 })
 
+// Listens on port 8090
 app.listen(8090);
