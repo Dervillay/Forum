d686e93fb9dc88f03c37ec9647dd02f53b61de88
Dervillay <james.gascoigneburns7@gmail.com>
2019-04-16 17:21:53 +0100

    Add secret for token authentication

---
+++ config.js
@@ -0,0 +1,4 @@
+/* Secret is stored in this file for demonstration purposes.
+ * Typically a secret would be stored locaal on a mahcine, but
+ * for ease of marking, it has been included here */
+const secret = "f67ab8499743d84ce0cc292a8c3de445060fa0ebc7f8b51e6f665a79270df2d0";
