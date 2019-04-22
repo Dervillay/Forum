eb1feca87d23224b1b0af7f99efea45205d04556
Dervillay <james.gascoigneburns7@gmail.com>
2019-04-22 12:02:25 +0100

    Add Google API as global

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
 	'env': {
 		'browser': true,
 		'es6': true,
-		'node': true
+		'node': true,
+		'jquery': true
 	},
 	'extends': 'eslint:recommended',
 	'globals': {
@@ -12,8 +13,14 @@ module.exports = {
 	'parserOptions': {
 		'ecmaVersion': 2018
 	},
+	'globals': {
+		"gapi": true // Sets Google API (gapi) as a global to avoid undefined error
+	},
 	'rules': {
-		'no-console': 'off',
+		'no-console':
+			'off', // Off since server logs user activity to server console
+		'no-unused-vars':
+			'off', // Off since some functions defined in index.js are only called in index.html
 		'indent': [
 			'error',
 			'tab'
