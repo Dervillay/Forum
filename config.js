e67a9681ef6ea8a231735eb7b8b8a078e97280d5
Dervillay <james.gascoigneburns7@gmail.com>
2019-04-25 21:42:41 +0100

    Change quotes to single to comply with eslint

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
  * Typically a secret would be stored locaal on a mahcine, but
  * for ease of marking, it has been included here */
 module.exports = {
-  "secret": "f67ab8499743d84ce0cc292a8c3de445060fa0ebc7f8b51e6f665a79270df2d0"
-}
+	'secret': 'f67ab8499743d84ce0cc292a8c3de445060fa0ebc7f8b51e6f665a79270df2d0'
+};
