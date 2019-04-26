71b4c637e5efa7e5c29b92cb42a1dace3518d0d2
Dervillay <james.gascoigneburns7@gmail.com>
2019-04-26 20:25:14 +0100

    Add safe shutdown on force close

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const app = require('./app');
 
 // Listens on recommended port with default of 8090
-app.listen(process.env.PORT || 8090, () => {
-	console.log('> Listening on port ' + (process.env.PORT || 8090));
+const server = app.listen(process.env.PORT || 8080, () => {
+	console.log('> Listening on port ' + (process.env.PORT || 8080));
+});
+
+process.on('SIGINT', () => {
+	console.log();
+	console.log('Closing HTTP server.');
+	server.close(() => {
+		console.log('HTTP server closed.');
+		process.exit(0);
+	});
 });
